685107dc84cf9d1c307cf45439d34f885fe44d26
Josh Turner <Josh.Turner.1@warwick.ac.uk>
2025-02-26 21:49:47 +0000

    optimal inner loop vectorisation

--- sparsemv.c
+++ sparsemv.c
@@ -25,24 +25,50 @@ int sparsemv(struct mesh *A, const double * const x, double * const y)
     const int * const cur_inds = (const int * const) A->ptr_to_inds_in_row[i];
     const int cur_nnz = (const int) A->nnz_in_row[i];
 
-    int j;
     __m256d sumVec = _mm256_setzero_pd();
-    for (j=0; j< cur_nnz-3; j+=4) {
+    int j = 0;
+    if(j< cur_nnz-15) {
       sumVec = _mm256_add_pd(
         sumVec,
-        _mm256_mul_pd(
-          _mm256_loadu_pd(cur_vals + j), 
-          _mm256_set_pd(x[cur_inds[j+3]], x[cur_inds[j+2]], x[cur_inds[j+1]], x[cur_inds[j]])
+        _mm256_add_pd(
+          _mm256_add_pd(
+            _mm256_mul_pd(_mm256_loadu_pd(cur_vals + j), _mm256_set_pd(x[cur_inds[j+3]], x[cur_inds[j+2]], x[cur_inds[j+1]], x[cur_inds[j]])),
+            _mm256_mul_pd(_mm256_loadu_pd(cur_vals + j + 4), _mm256_set_pd(x[cur_inds[j+7]], x[cur_inds[j+6]], x[cur_inds[j+5]], x[cur_inds[j+4]]))
+          ),
+          _mm256_add_pd(
+            _mm256_mul_pd(_mm256_loadu_pd(cur_vals + j + 8), _mm256_set_pd(x[cur_inds[j+11]], x[cur_inds[j+10]], x[cur_inds[j+9]], x[cur_inds[j+8]])),
+            _mm256_mul_pd(_mm256_loadu_pd(cur_vals + j + 12), _mm256_set_pd(x[cur_inds[j+15]], x[cur_inds[j+14]], x[cur_inds[j+13]], x[cur_inds[j+12]]))
+          )
         )
       );
+      j+=16;
     }
-    
+
+    if (j< cur_nnz-7) {
+      sumVec = _mm256_add_pd(
+        sumVec,
+        _mm256_add_pd(
+          _mm256_mul_pd(_mm256_loadu_pd(cur_vals + j), _mm256_set_pd(x[cur_inds[j+3]], x[cur_inds[j+2]], x[cur_inds[j+1]], x[cur_inds[j]])),
+          _mm256_mul_pd(_mm256_loadu_pd(cur_vals + j + 4), _mm256_set_pd(x[cur_inds[j+7]], x[cur_inds[j+6]], x[cur_inds[j+5]], x[cur_inds[j+4]]))
+        )
+      );
+      j+=8;
+    }
+
+    if (j< cur_nnz-3) {
+      sumVec = _mm256_add_pd(
+        sumVec,
+        _mm256_mul_pd(_mm256_loadu_pd(cur_vals + j), _mm256_set_pd(x[cur_inds[j+3]], x[cur_inds[j+2]], x[cur_inds[j+1]], x[cur_inds[j]]))
+      );
+      j+=4;
+    }
+
     double sum = 0.0;
     for (; j< cur_nnz; j++) {
       sum += cur_vals[j] * x[cur_inds[j]];
     }
 
-    y[i] = sumVec[0] + sumVec[1] + sumVec[2] + sumVec[3] + sum;
+    y[i] = sum + sumVec[0] + sumVec[1] + sumVec[2] + sumVec[3];
   }
   return 0;
 }
