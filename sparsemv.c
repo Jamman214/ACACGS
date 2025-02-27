624b72bf76ffb2078e7b1e50c80448d0f28a7e29
Josh Turner <Josh.Turner.1@warwick.ac.uk>
2025-02-27 14:32:59 +0000

    added shared memory for storing sums

--- sparsemv.c
+++ sparsemv.c
@@ -18,57 +18,66 @@
 int sparsemv(struct mesh *A, const double * const x, double * const y)
 {
 
+  double* sums = (double*)_mm_malloc(sizeof(double) * 4 * 4, 32);
   const int nrow = (const int) A->local_nrow;
-  #pragma omp parallel for
-  for (int i=0; i< nrow; i++) {
-    const double * const cur_vals = (const double * const) A->ptr_to_vals_in_row[i];
-    const int * const cur_inds = (const int * const) A->ptr_to_inds_in_row[i];
-    const int cur_nnz = (const int) A->nnz_in_row[i];
+  #pragma omp parallel num_threads(4)
+  {
+    double* const threadSum = sums + (4 * omp_get_thread_num());
 
-    __m256d sumVec = _mm256_setzero_pd();
-    int j = 0;
-    if(j< cur_nnz-15) {
-      sumVec = _mm256_add_pd(
-        sumVec,
-        _mm256_add_pd(
+    #pragma omp for
+    for (int i=0; i< nrow; i++) {
+      const double * const cur_vals = (const double * const) A->ptr_to_vals_in_row[i];
+      const int * const cur_inds = (const int * const) A->ptr_to_inds_in_row[i];
+      const int cur_nnz = (const int) A->nnz_in_row[i];
+      
+
+      __m256d sumVec = _mm256_setzero_pd();
+      int j = 0;
+      if(j< cur_nnz-15) {
+        sumVec = _mm256_add_pd(
+          sumVec,
+          _mm256_add_pd(
+            _mm256_add_pd(
+              _mm256_mul_pd(_mm256_loadu_pd(cur_vals + j), _mm256_set_pd(x[cur_inds[j+3]], x[cur_inds[j+2]], x[cur_inds[j+1]], x[cur_inds[j]])),
+              _mm256_mul_pd(_mm256_loadu_pd(cur_vals + j + 4), _mm256_set_pd(x[cur_inds[j+7]], x[cur_inds[j+6]], x[cur_inds[j+5]], x[cur_inds[j+4]]))
+            ),
+            _mm256_add_pd(
+              _mm256_mul_pd(_mm256_loadu_pd(cur_vals + j + 8), _mm256_set_pd(x[cur_inds[j+11]], x[cur_inds[j+10]], x[cur_inds[j+9]], x[cur_inds[j+8]])),
+              _mm256_mul_pd(_mm256_loadu_pd(cur_vals + j + 12), _mm256_set_pd(x[cur_inds[j+15]], x[cur_inds[j+14]], x[cur_inds[j+13]], x[cur_inds[j+12]]))
+            )
+          )
+        );
+        j+=16;
+      }
+
+      if (j< cur_nnz-7) {
+        sumVec = _mm256_add_pd(
+          sumVec,
           _mm256_add_pd(
             _mm256_mul_pd(_mm256_loadu_pd(cur_vals + j), _mm256_set_pd(x[cur_inds[j+3]], x[cur_inds[j+2]], x[cur_inds[j+1]], x[cur_inds[j]])),
             _mm256_mul_pd(_mm256_loadu_pd(cur_vals + j + 4), _mm256_set_pd(x[cur_inds[j+7]], x[cur_inds[j+6]], x[cur_inds[j+5]], x[cur_inds[j+4]]))
-          ),
-          _mm256_add_pd(
-            _mm256_mul_pd(_mm256_loadu_pd(cur_vals + j + 8), _mm256_set_pd(x[cur_inds[j+11]], x[cur_inds[j+10]], x[cur_inds[j+9]], x[cur_inds[j+8]])),
-            _mm256_mul_pd(_mm256_loadu_pd(cur_vals + j + 12), _mm256_set_pd(x[cur_inds[j+15]], x[cur_inds[j+14]], x[cur_inds[j+13]], x[cur_inds[j+12]]))
           )
-        )
-      );
-      j+=16;
-    }
+        );
+        j+=8;
+      }
 
-    if (j< cur_nnz-7) {
-      sumVec = _mm256_add_pd(
-        sumVec,
-        _mm256_add_pd(
-          _mm256_mul_pd(_mm256_loadu_pd(cur_vals + j), _mm256_set_pd(x[cur_inds[j+3]], x[cur_inds[j+2]], x[cur_inds[j+1]], x[cur_inds[j]])),
-          _mm256_mul_pd(_mm256_loadu_pd(cur_vals + j + 4), _mm256_set_pd(x[cur_inds[j+7]], x[cur_inds[j+6]], x[cur_inds[j+5]], x[cur_inds[j+4]]))
-        )
-      );
-      j+=8;
-    }
+      if (j< cur_nnz-3) {
+        sumVec = _mm256_add_pd(
+          sumVec,
+          _mm256_mul_pd(_mm256_loadu_pd(cur_vals + j), _mm256_set_pd(x[cur_inds[j+3]], x[cur_inds[j+2]], x[cur_inds[j+1]], x[cur_inds[j]]))
+        );
+        j+=4;
+      }
 
-    if (j< cur_nnz-3) {
-      sumVec = _mm256_add_pd(
-        sumVec,
-        _mm256_mul_pd(_mm256_loadu_pd(cur_vals + j), _mm256_set_pd(x[cur_inds[j+3]], x[cur_inds[j+2]], x[cur_inds[j+1]], x[cur_inds[j]]))
-      );
-      j+=4;
-    }
+      _mm256_store_pd(threadSum, sumVec);
 
-    double sum = 0.0;
-    for (; j< cur_nnz; j++) {
-      sum += cur_vals[j] * x[cur_inds[j]];
-    }
+      for (; j< cur_nnz; j++) {
+        threadSum[0] += cur_vals[j] * x[cur_inds[j]];
+      }
 
-    y[i] = sumVec[0] + sumVec[1] + sumVec[2] + sumVec[3] + sum;
+      y[i] = threadSum[0] + threadSum[1] + threadSum[2] + + threadSum[3];
+    }
   }
+  _mm_free(sums);
   return 0;
 }
