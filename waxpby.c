5726a50ba062351323d3cd6eb218dd1c5f9912ad
Josh Turner <Josh.Turner.1@warwick.ac.uk>
2025-03-06 03:15:40 +0000

    inlined vectorisation

--- waxpby.c
+++ waxpby.c
@@ -15,11 +15,10 @@
  * @return int 0 if no error
  */
 int waxpby (const int n, const double * const x, const double beta, const double * const y, double * const w) {  
-  __m256d betaVec = _mm256_set1_pd(beta);
   int parallelN = (n/4)*4;
   #pragma omp parallel for num_threads(4)
   for (int i=0; i<parallelN; i+=4) {
-    _mm256_store_pd(w+i, _mm256_add_pd(_mm256_load_pd(x+i), _mm256_mul_pd(betaVec, _mm256_load_pd(y+i))));
+    _mm256_store_pd(w+i, _mm256_add_pd(_mm256_load_pd(x+i), _mm256_mul_pd(_mm256_set1_pd(beta), _mm256_load_pd(y+i))));
   }
   for (int i=parallelN; i<n; i++) {
     w[i] = x[i] + beta * y[i];
